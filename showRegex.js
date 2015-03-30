6340ca068624b17e5128a64286fd21ee6e1bba0f
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-03-30 13:06:36 -0400

    Initial Commit for the problem

---
+++ showRegex.js
@@ -0,0 +1,13 @@
+// Coded by Jared Smith
+// March 30, 2015
+// showRegex problem from the practicum
+
+var sentence1 = "Hello, my name is Opie!"
+var sentence2 = "My friends call me Jared!"
+
+var reg1 = /Opie/
+var reg2 = new RegExp("Jared")
+
+console.log(sentence1);
+console.log(sentence2);
+
