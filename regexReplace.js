a5fc5291affcc01af12167c67a05f7ecc4182e06
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-03-30 12:31:40 -0400

    Initial Commit

---
+++ regexReplace.js
@@ -0,0 +1,7 @@
+// Coded by Jared Smith
+// March 30, 2015
+// Practicum for Test2 RegexReplace problem
+
+var words = "Today I, Opie, will attempt to solve world hunger. When I, Opie, solve world hunger everyone will adore me shouting out, 'Oh look! There goes Opie! The boy who solved world hunger.'";
+
+console.log(words);
