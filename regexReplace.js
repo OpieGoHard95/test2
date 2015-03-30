d7fc4ee1985e0c22e6fedadaa2ce33de3eee0754
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-03-30 13:00:39 -0400

    Finished the problem :)

--- regexReplace.js
+++ regexReplace.js
@@ -4,6 +4,6 @@
 
 var words = "Today I, Opie, will attempt to solve world hunger. When I, Opie, solve world hunger everyone will adore me shouting out, 'Oh look! There goes Opie! The boy who solved world hunger.'";
 
-
+// The regex should look through the string and find Opie and put Jared in
 
 console.log(words.replace(/\b(Opie)\b/g, "Jared"));
