264f9fd33bbbdc18959f761b6622e2f9cb46e3f5
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-03-30 12:36:28 -0400

    Added regex replace method

--- regexReplace.js
+++ regexReplace.js
@@ -4,4 +4,6 @@
 
 var words = "Today I, Opie, will attempt to solve world hunger. When I, Opie, solve world hunger everyone will adore me shouting out, 'Oh look! There goes Opie! The boy who solved world hunger.'";
 
-console.log(words);
+
+
+console.log(words.replace(/\b(Opie)\b/g, "Jared"));
